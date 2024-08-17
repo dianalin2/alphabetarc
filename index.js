28842cad1adb034d7f7eeb29d95b1cf38f6d090c
Diana Lin <lindiana1206@gmail.com>
2024-08-17 00:35:28 -0400

    fix: add client rewrite to serve /*

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use("/api/v1", apiRouter);
 
 app.use(express.static("client/build"));
 
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/client/build/index.html");
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
